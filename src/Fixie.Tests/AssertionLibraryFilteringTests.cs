11c30d6d464165547361d2fa144add5fa1c56f76
Bernhard Richter <bernhard.richter@gmail.com>
2015-02-04 13:58:12 +0100

    Culture independant decimal separator

--- src/Fixie.Tests/AssertionLibraryFilteringTests.cs
+++ src/Fixie.Tests/AssertionLibraryFilteringTests.cs
@@ -79,12 +79,9 @@ namespace Fixie.Tests
         {
             //Avoid brittle assertion introduced by fixie version.
             var cleaned = Regex.Replace(actualRawContent, @"\(Fixie \d+\.\d+\.\d+\.\d+\)", @"(Fixie 1.2.3.4)");
-
+            
+            //Avoid brittle assertion introduced by test duration.            
             var decimalSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
-
-            //Avoid brittle assertion introduced by test duration.
-            //cleaned = Regex.Replace(cleaned, @"took [\d\.]+ seconds", @"took 1.23 seconds");
-
             cleaned = Regex.Replace(cleaned, @"took [\d\" + decimalSeparator + "]+ seconds", @"took 1.23 seconds");
 
             //Avoid brittle assertion introduced by stack trace line numbers.
